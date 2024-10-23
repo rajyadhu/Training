f2cf56a30dd83efb321071b8ab84ebe573970a17
rajyadhu <136621342+rajyadhu@users.noreply.github.com>
2024-10-23 19:51:10 +0530

    Add files via upload

---
+++ Day 21/Controllers/AccountController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Sign_up_form.DAL;
+using Sign_up_form.Models;
+
+namespace Sign_up_form.Controllers
+{
+    public class AccountController : Controller
+    {
+        private User_DAL userDAL = new User_DAL();
+
+        // Load the main page and the list of registered users
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SignUp form and registered users list
+        public ActionResult SignUp()
+        {
+            List<User> users = userDAL.Get_all_users();  // Get all users from DAL
+            ViewBag.Users = users;
+            return View(new User()); // Pass an empty User model for sign-up
+        }
+
+        // POST: Handle form submission for sign-up
+        [HttpPost]
+        public ActionResult SignUp(User newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                userDAL.AddUser(newUser);  // Add the new user to the database
+                return RedirectToAction("SignUp"); // Redirect to the same page to show updated list
+            }
+
+            // If model state is invalid, return to the same view with the form data
+            List<User> users = userDAL.Get_all_users();
+            ViewBag.Users = users;
+            return View(newUser);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteUser(string username)
+        {
+            userDAL.DeleteUser(username);  // Remove the user from the database
+            return RedirectToAction("SignUp");  // Redirect to refresh the list
+        }
+
+        // GET: Load the Edit User page
+        public ActionResult EditUser(string username)
+        {
+            var user = userDAL.GetUserByUsername(username);
+
+            if (user == null)
+            {
+                return HttpNotFound(); // Handle case where user is not found
+            }
+
+            return View(user); // Pass the user model to the EditUser view
+        }
+
+        // POST: Handle the update of the user
+        [HttpPost]
+        public ActionResult EditUser(User updatedUser)
+        {
+            if (ModelState.IsValid)
+            {
+                userDAL.EditUser(updatedUser); // Update the user in the database
+                return RedirectToAction("SignUp"); // Redirect to the sign-up page after update
+            }
+
+            // If model state is invalid, return to the same view with the form data
+            return View(updatedUser); // Stay on the edit page to correct errors
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Day 21/Repository/User_DAL.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Sign_up_form.Models;
+
+
+
+namespace Sign_up_form.DAL
+{
+    public class User_DAL
+    {
+        string connection_string = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+
+        //Get all users
+        public List<User> Get_all_users()
+        {
+            List<User> usersList = new List<User>();
+
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "get_all_users";
+                SqlDataAdapter sqlDA = new SqlDataAdapter(command);
+                DataTable dtUsers = new DataTable();
+
+                connection.Open();
+                sqlDA.Fill(dtUsers);
+                connection.Close();
+
+                foreach (DataRow dr in dtUsers.Rows)
+                {
+                    usersList.Add(new User
+                    {
+                        FirstName = dr["FirstName"].ToString(),
+                        LastName = dr["LastName"].ToString(),
+                        Username = dr["Username"].ToString(),
+                        Place = dr["Place"].ToString(),
+                        PhoneNo = dr["PhoneNo"].ToString()
+                    });
+                }
+            }
+            return usersList;
+        }
+
+        public void AddUser(User newUser)
+        {
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+                SqlCommand command = new SqlCommand("AddUser", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@FirstName", newUser.FirstName);
+                command.Parameters.AddWithValue("@LastName", newUser.LastName);
+                command.Parameters.AddWithValue("@Username", newUser.Username);
+                command.Parameters.AddWithValue("@Place", newUser.Place);
+                command.Parameters.AddWithValue("@PhoneNo", newUser.PhoneNo);
+                command.Parameters.AddWithValue("@Password", newUser.Password);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        public void EditUser(User updatedUser)
+        {
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+                SqlCommand command = new SqlCommand("EditUser", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@FirstName", updatedUser.FirstName);
+                command.Parameters.AddWithValue("@LastName", updatedUser.LastName);
+                command.Parameters.AddWithValue("@Username", updatedUser.Username);
+                command.Parameters.AddWithValue("@Place", updatedUser.Place);
+                command.Parameters.AddWithValue("@PhoneNo", updatedUser.PhoneNo);
+                command.Parameters.AddWithValue("@Password", updatedUser.Password);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        public void DeleteUser(string username)
+        {
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+                SqlCommand command = new SqlCommand("DeleteUser", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Username", username);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        public User GetUserByUsername(string username)
+        {
+            User user = null;
+
+            using (SqlConnection connection = new SqlConnection(connection_string))
+            {
+                SqlCommand command = new SqlCommand("SELECT UserId, FirstName, LastName, Username, Place, PhoneNo, Password  FROM Users WHERE Username = @Username", connection);
+                command.Parameters.AddWithValue("@Username", username);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read()) // Check if a record was found
+                {
+                    user = new User
+                    {
+                        UserId = Convert.ToInt32(reader["UserId"]),
+                        FirstName = reader["FirstName"].ToString(),
+                        LastName = reader["LastName"]?.ToString(), // Nullable check
+                        Username = reader["Username"].ToString(),
+                        Place = reader["Place"].ToString(),
+                        PhoneNo = reader["PhoneNo"].ToString(),
+                        Password = reader["Password"].ToString()
+                    };
+                }
+
+                reader.Close();
+                connection.Close();
+            }
+
+            return user; // Return the user object or null if not found
+        }
+
+
+    }
+}
